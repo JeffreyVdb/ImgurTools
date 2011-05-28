e00ee2d37893ca9c59fd8a7e7c8a57e710e7e9f4
Jeffrey Vandenborne <jeffreyv@illutek.com>
2011-05-28 23:34:27 +0200

    Revert "Added progress"
    This reverts commit 0e789527697312fe89d17b103d626c66d7c19c42.

--- src/perl/imgerl.pl
+++ src/perl/imgerl.pl
@@ -4,14 +4,12 @@ use Getopt::Long;
 use Pod::Usage;
 use LWP::UserAgent;
 use JSON;
-use threads;
-use threads::shared;
-use Time::HiRes qw/usleep/;
 use MIME::Base64;
 
 my $version = '1.0';
 my $api_url = 'http://api.imgur.com/2/upload.json';
 my $api_key = 'cea84480cf3f38814b991094fb8be8ed';
+my $img_prefix = 'http://i.imgur.com/';
 
 my @exts      = ('png','jpg', 'gif');
 my $verbose   = undef;
@@ -22,7 +20,7 @@ my $help      = 0;
 my $file      = undef;
 
 sub usage {
-    open my $fh, '<', './documentation.pod';
+    open my $fh, '<', 'documentation.pod';
     pod2usage(
 	-exitval => 1,
 	-input	 => $fh,
@@ -37,11 +35,6 @@ sub upload_file {
     # We need one argument, always
     return unless $_[0];
 
-    # Turn buffering off for progress
-    local $| = 1;
-
-    my $ufile :shared = $_[0];
-    my $finished :shared = 0;
     my $ua    =	 LWP::UserAgent->new;
     my $bufenc;
     open my $IMGF, '<', $_[0];
@@ -54,26 +47,12 @@ sub upload_file {
     }
     close $IMGF;
 
-    # Let's show the user a progress bar..
-    my $thr = async {
-	my @progs = ('/', '-', '\\', '|');
-	my $i = 0;
-	my $message = 'uploading ' . $ufile . ' ';
-
-	print $message;
-	until ($finished) {
-	    print $progs[$i++];
-	    $i = 0 if $i == @progs;
-
-	    usleep(50000);
-	    print "\b";
-	}
-
-	print "... done\n";
-    };
-
-    my $resp = $ua->post($api_url, { key => $api_key, image =>
-	$bufenc, type => 'base64', name => $_[0] });
+    my $resp  =	 $ua->post($api_url, {
+	key   => $api_key,
+	image => $bufenc,
+	type => 'base64',
+	name => $_[0]
+    });
 
     unless ($resp->is_success) {
 	print "Failed to fetch\n";
@@ -82,11 +61,6 @@ sub upload_file {
 
     my $json_data = from_json($resp->content);
     my %values = %{$json_data->{'upload'}{'links'}};
-
-    # Tell progress to stop
-    $finished = 1;
-    $| = 0;
-    $thr->join;
 
     print $values{'original'}, "\n";
     print "\n";
